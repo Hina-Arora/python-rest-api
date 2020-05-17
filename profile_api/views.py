015dc1ffcc92314b9f35d158cd80ddee8d085b05
HinaArora <hinaarora23050@gmail.com>
2020-05-17 22:36:32 +0530

    added searching and filter

--- profile_api/views.py
+++ profile_api/views.py
@@ -2,8 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
+from profile_api import permissions
 from profile_api import serializers
+from profile_api import models
+
 
 
 class HelloApiView(APIView):
@@ -93,3 +98,14 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """handle destroy the object"""
         return Response({'http_method':'DESTROY'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """handling creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
